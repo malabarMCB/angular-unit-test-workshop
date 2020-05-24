1854e433de3dec5d2ba098e20bd83f40430eac61
Dmytro Hrynevych <grinevich97@gmail.com>
2020-05-24 17:34:33 +0300

    Add service test

--- workshop/src/app/services/service.spec.ts
+++ workshop/src/app/services/service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, inject, TestBed, tick} from '@angular/core/testing';
 
 import { Service } from './service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('Service', () => {
-  let service: Service;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Service);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [Service]
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  function mockResponse(backend: HttpTestingController, url: string, body: any) {
+    const testRequest = backend.expectOne(url);
+    testRequest.flush(body);
+    return testRequest;
+  }
+
+  it('should get message from backend', inject(
+    [Service, HttpTestingController],
+    fakeAsync((service, backend) => {
+
+      const expectedMessage = 'expected text';
+      let actualMessage;
+      service.getMessage().subscribe(x => actualMessage = x);
+      mockResponse(backend, 'http://localhost:4545/message', {text: expectedMessage});
+
+      tick();
+
+      expect(actualMessage).toBe(expectedMessage);
+      backend.verify();
+    })));
 });
